6a9058db2ab3a4d4dfdc871370aab866f38c54a0
AniketAltair <anikadh01@gmail.com>
2025-03-08 01:13:12 +0530

    Setup commit

---
+++ src/main/java/com/example/macrominder/authService/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.macrominder.authService.controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+    @GetMapping("v1/hello")
+    public String helloWorld() {
+        System.out.println("Hello");
+        return "Hello, World!";
+    }
+}
